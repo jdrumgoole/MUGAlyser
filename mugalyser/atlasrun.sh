64c28017cb9611e13e271ba0bc2dfb16cc0caf40
Joe Drumgoole <joe.drumgoole@mongodb.com>
2016-10-11 15:12:56 +0100

    Added replset

--- mugalyser/atlasrun.sh
+++ mugalyser/atlasrun.sh
@@ -1,1 +1,1 @@
-python mugalyser_main.py --host mugalyser-shard-00-00-ffp4c.mongodb.net --username jdrumgoole --ssl $*
+python mugalyser_main.py --replset MUGAlyser-shard-0 --host mugalyser-shard-00-00-ffp4c.mongodb.net --username jdrumgoole --ssl $*
