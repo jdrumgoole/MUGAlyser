4b299358330b7f769a291529125694bd66d7cb46
Joe Drumgoole <joe.drumgoole@mongodb.com>
2016-10-24 15:05:13 +0100

    added other commands

--- fabfile.py
+++ fabfile.py
@@ -3,10 +3,11 @@
 from fabric.api import run, env, local,cd
 
 import os
+import json
 
 from mugalyser.apikey import MEETUP_API_KEY, PASSWD
 
-env.hosts = ['ec2-52-50-135-182.eu-west-1.compute.amazonaws.com']
+env.hosts = ["ec2-52-212-52-126.eu-west-1.compute.amazonaws.com"]
 env.user = 'ec2-user'
 home = os.getenv( "HOME" )
 env.key_filename = os.path.join( home, "Documents", "jdrumgoole-vosa-eu.pem" )
@@ -25,15 +26,26 @@ def local_uname():
 def remote_uname():
     run('uname -a')
 
+def describe_instance():
+    local( "aws ec2 describe-instances --instance-ids %s" % instance_id )
+    
+def get_dns_name():
+    info = local( "aws ec2 describe-instances --instance-ids %s" % instance_id, capture=True )
+    #print( "--%s--" % info )
+    instance_info = json.loads( info )
+    env.hosts.append( instance_info["Reservations"][0]["Instances"][0]["PublicDnsName"] )
+    print( "Host: %s" % instance_info["Reservations"][0]["Instances"][0]["PublicDnsName"])
+    
 def start_instance():
     local( "aws ec2 start-instances --instance-id %s" % instance_id )
-    local( "aws wait instance-running --instance-ids %s" % instance_id )
+    local( "aws ec2  wait instance-running --instance-ids %s" % instance_id )
+    get_dns_name()
     
 def stop_instance():
     local( "aws ec2 stop-instances --instance-id %s" % instance_id )
-    
 
 def update() :
+    print( env.hosts )
     run( "sudo yum update" )
 
 def gitpull() :
@@ -57,3 +69,19 @@ def attendees():
     with cd( "GIT/MUGAlyser/mugalyser") :
         run("python mugalyser_main.py --replset MUGAlyser-shard-0 --host %s --username jdrumgoole --ssl --password %s --attendees all" % ( ATLAS_PRIMARY, PASSWD ))     
     
+def process_batch():
+    start_instance()
+    #get_dns_name()
+    update()
+    gitpull()
+    mugs()
+    attendees()
+    stop_instance()
+    
+def test_batch():
+    get_dns_name()
+    update()
+    gitpull()
+    mugs()
+    attendees()
+    stop_instance()
