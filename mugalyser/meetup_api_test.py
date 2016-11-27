5b5b709e2aa4c1e9e16e5dead02af5ce711cf298
Joe Drumgoole <joe.drumgoole@mongodb.com>
2016-11-27 23:58:22 +0000

    Started unit tests

---
+++ mugalyser/meetup_api_test.py
@@ -0,0 +1,18 @@
+'''
+Created on 26 Nov 2016
+
+@author: jdrumgoole
+'''
+import unittest
+
+
+class MeetupAPI_Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ mugalyser/members_test.py
@@ -0,0 +1,26 @@
+'''
+Created on 26 Nov 2016
+
+@author: jdrumgoole
+'''
+import unittest
+
+
+class TestMembers(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
