d92b2f365dd58bfb608e5669482a5f6deafd33b7
Joe Drumgoole <joe.drumgoole@mongodb.com>
2016-12-12 16:07:08 +0000

    Scrapped password parameter

--- mugalyser/apikey_template.py
+++ mugalyser/apikey_template.py
@@ -1,10 +1,6 @@
 MEETUP_API_KEY=""
-MUGALYSER_PASSWD=""
 
 import os
 
 def get_meetup_key( default=MEETUP_API_KEY ):
-    return os.getenv( "MEETUP_AP_KEY", default )
-
-def get_mugalyer_passwd( default=MUGALYSER_PASSWD ):
-    return os.getenv( "MUGALYSER_PASSWD", default )
+    return os.getenv( "MEETUP_API_KEY", default )
--- mugalyser/makeapikeyfile_main.py
+++ mugalyser/makeapikeyfile_main.py
@@ -5,20 +5,35 @@ Created on 30 Sep 2016
 '''
 
 import sys
+from argparse import ArgumentParser
+
 from datetime import datetime
+
 if __name__ == '__main__':
     
-    key = sys.argv[ 1 ]
-    passwd = sys.argv[ 2 ]
+    
+    parser = ArgumentParser()
+    parser.add_argument( "-a", "--apikey", default="", help="Meetup API key")
+    
+    args = parser.parse_args()
+    
+    apikey = args.apikey
+    
+    if ( apikey == "" ) :
+        print( "Warning no argument to --apikey, have you defined MEETUP_API_KEY in the environment?")
+        print( "Goto https://secure.meetup.com/meetup_api/key/ to get an API key for meetup.com")
 
     with open( "apikey_template.py", "r") as infile:
         with open( "apikey.py", "w" ) as output:
             output.write( "# Created: %s\n" % datetime.now())
             for line in infile :
                 if line.startswith( "MEETUP_API_KEY"):
-                    output.write( "MEETUP_API_KEY=\"%s\"\n" % key )
-                elif line.startswith( "MUGALYSER_PASSWD"):
-                    output.write( "MUGALYSER_PASSWD=\"%s\"\n" % passwd )
+                    output.write( "MEETUP_API_KEY=\"%s\"\n" % apikey )
                 else:
                     output.write( line )
+                    
+    if apikey == "" :
+        sys.exit( 2 )
+    else:
+        sys.exit( 0 )
 
