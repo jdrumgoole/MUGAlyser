ec7077f3595fc149db509974e04e17b428b8c0dc
Joe Drumgoole <joe.drumgoole@mongodb.com>
2017-02-07 01:21:12 +0000

    Pydrive test code

--- tests/pydrive_test.py
+++ tests/pydrive_test.py
@@ -4,8 +4,10 @@ Created on 23 Jan 2017
 @author: jdrumgoole
 '''
 from pydrive import auth
+from pydrive.drive import GoogleDrive
 from argparse import ArgumentParser
-
+import pprint
+import json
 
 if __name__ == '__main__':
     
@@ -18,10 +20,21 @@ if __name__ == '__main__':
     
     gauth.LoadClientConfigFile( args.config )
     
-    from pydrive.drive import GoogleDrive
+
 
     drive = GoogleDrive(gauth)
     
+    
+    files = drive.ListFile()
+    
+    for i in files:
+        s = json.dumps( i )
+        #pprint.pprint( s )
+        
+        for j in i:
+            pprint.pprint( j["title"] )
+
+        
     file1 = drive.CreateFile({'title': 'Hello-gdrive.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
     file1.SetContentString('Hello World!') # Set content of the file from given string.
     file1.Upload()
