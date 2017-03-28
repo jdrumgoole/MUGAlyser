2cec5c15f7a1154ad42e3c67cd801b94166a84a4
Joe Drumgoole <joe.drumgoole@mongodb.com>
2017-03-28 15:51:58 +0100

    prog to run progs against atlas

---
+++ apps/run_prog.sh
@@ -0,0 +1,5 @@
+PROG=$1
+shift
+PASSWORD=$2
+shift
+python $PROG --host "mongodb://jdrumgoole:${PASSWORD}@mugalyser-shard-00-00-ffp4c.mongodb.net:27017,mugalyser-shard-00-01-ffp4c.mongodb.net:27017,mugalyser-shard-00-02-ffp4c.mongodb.net:27017/MUGS?ssl=true&replicaSet=MUGAlyser-shard-0&authSource=admin" $*
