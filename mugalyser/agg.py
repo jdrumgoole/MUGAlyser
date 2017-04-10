bd2b90ebd245b7598ef2c77b6838e00320b2e2e6
Joe Drumgoole <joe.drumgoole@mongodb.com>
2017-04-10 12:24:01 +0100

    Moved nested dict into its own class

--- mugalyser/agg.py
+++ mugalyser/agg.py
@@ -6,7 +6,7 @@ import pprint
 from datetime import datetime
 from collections import OrderedDict
 import collections
-
+from mugalyser.nested_dict import nested_dict
 import pymongo
 import csv
 import contextlib
@@ -58,79 +58,6 @@ class Sorter( object ):
     
     def __repr__(self):
         return self.__str__()
-    
-class NestedDict( object ):
-    '''
-    Allow dotted access of nested dicts.
-    so :
-    a[ "x.y.z" ] = 1 is equivalent to a[ x[ y[ z ]]] = 1
-    
-    a = { x : { y : z }}}
-    '''
-    
-    def __init__(self, d={}):
-        
-        if isinstance( d, dict ):
-            self._dict = d
-        else:
-            raise ValueError( "d is not a dict type")
-
-    def dict_value(self ):
-        return self._dict
-    
-    def get_value( self, k ):
-        keys = k.split( ".", 1 )
-        
-        #print( "get_value %s" % k  )
-        if len( keys ) == 1 :
-            #print( "len(keys) : 1")
-            return self._dict[ keys[ 0 ]]
-        elif self._dict.has_key( keys[ 0 ]) :
-                nested = self._dict[ keys[ 0 ]]
-        else:
-            raise ValueError( "nested key :'%s' does not exist in keys %s of %s" % ( keys[ 0 ], keys, self._dict ))
-        
-        if isinstance( nested, dict ) :
-            nested = NestedDict( nested )
-            return nested.get_value( keys[ 1 ] )
-        else:
-            return nested
-    
-    def has_key( self, k ):
-        
-        keys = k.split( ".", 1 )
-        nested = None
-        if len( keys ) == 1 :
-            return self._dict.has_key( keys[ 0 ] )
-        elif self._dict.has_key( keys[ 0 ]) :
-                nested = self._dict[ keys[ 0 ]]
-        else:
-            raise ValueError( "nested key :'%s' does not exist" % keys[ 0 ])
-        
-        if isinstance( nested, dict ) :
-            nested = NestedDict( nested )
-            return nested.has_key( keys[ 1 ] )
-        else:
-            return True
-                      
-        
-    def set_value( self, k, v ):
-        
-        keys = k.split( ".", 1 )
-        nested = None
-        if len( keys ) == 1 :
-            self._dict[ keys[ 0 ]] = v
-            return self
-        elif self._dict.has_key( keys[ 0 ]) :
-            nested = self._dict[ keys[ 0 ]]
-        else:
-            self._dict[ keys[ 0 ]] = {}
-            nested = self._dict[ keys[ 0 ]]
-        
-        if isinstance( nested, dict ) :
-            nested = NestedDict( nested )
-            nested.set_value( keys[ 1 ], v )
-            return self
         
 class CursorFormatter( object ):
     '''
@@ -174,7 +101,7 @@ class CursorFormatter( object ):
         Given a field that contains a datetime 
         '''
         
-        d = NestedDict( doc )
+        d = nested_dict( doc )
         value = d.get_value(  field )
         if isinstance( value, datetime ):
             d.set_value( field, value.strftime( time_format ) )
@@ -193,8 +120,8 @@ class CursorFormatter( object ):
         if fields is None or len( fields ) == 0 :
             return doc
         
-        newDoc = NestedDict( {} )
-        oldDoc = NestedDict( doc )
+        newDoc = nested_dict( {} )
+        oldDoc = nested_dict( doc )
 
         
         for i in fields:
---
+++ mugalyser/nested_dict.py
@@ -0,0 +1,149 @@
+'''
+Created on 4 Apr 2017
+
+@author: jdrumgoole
+'''
+    
+class nested_dict( object ):
+    '''
+    Allow dotted access of nested dicts.
+    so :
+    a[ "x.y.z" ] = 1 is equivalent to a[ x[ y[ z ]]] = 1
+    
+    a = { x : { y : z }}}
+    '''
+    
+    def __init__(self, d={}):
+        
+        if isinstance( d, dict ):
+            self._dict = d
+        else:
+            raise ValueError( "d is not a dict type")
+
+    def dict_value(self ):
+        return self._dict
+    
+    def get_value( self, k ):
+        keys = k.split( ".", 1 )
+        
+        #print( "get_value %s" % k  )
+        if len( keys ) == 1 :
+            #print( "len(keys) : 1")
+            return self._dict[ keys[ 0 ]]
+        elif self._dict.has_key( keys[ 0 ]) :
+                nested = self._dict[ keys[ 0 ]]
+        else:
+            raise ValueError( "nested key :'%s' does not exist in keys %s of %s" % ( keys[ 0 ], keys, self._dict ))
+        
+        if isinstance( nested, dict ) :
+            nested = nested_dict( nested )
+            return nested.get_value( keys[ 1 ] )
+        else:
+            return nested
+    
+    def has_key( self, k ):
+        
+        keys = k.split( ".", 1 )
+        nested = None
+        if len( keys ) == 1 :
+            return self._dict.has_key( keys[ 0 ] )
+        elif self._dict.has_key( keys[ 0 ]) :
+                nested = self._dict[ keys[ 0 ]]
+        else:
+            raise ValueError( "nested key :'%s' does not exist" % keys[ 0 ])
+        
+        if isinstance( nested, dict ) :
+            nested = nested_dict( nested )
+            return nested.has_key( keys[ 1 ] )
+        else:
+            return True
+                      
+        
+    def set_value( self, k, v ):
+        
+        keys = k.split( ".", 1 )
+        nested = None
+        if len( keys ) == 1 :
+            self._dict[ keys[ 0 ]] = v
+            return self
+        elif self._dict.has_key( keys[ 0 ]) :
+            nested = self._dict[ keys[ 0 ]]
+        else:
+            self._dict[ keys[ 0 ]] = {}
+            nested = self._dict[ keys[ 0 ]]
+        
+        if isinstance( nested, dict ) :
+            nested = NestedDict( nested )
+            nested.set_value( keys[ 1 ], v )
+            return self
+
+
+
+class dotted_dict( dict ):
+    '''
+    Allow dotted access of nested dicts.
+    so :
+    a[ "x.y.z" ] = 1 is equivalent to a[ x[ y[ z ]]] = 1
+    
+    a = { x : { y : z }}}
+    '''
+    def __init__( self, *args, **kwargs ):
+        super( dotted_dict, self).__init__( *args, **kwargs )
+
+
+    def dict_value(self ):
+        return self._dict
+    
+    def get_value( self, k ):
+        keys = k.split( ".", 1 )
+        
+        #print( "get_value %s" % k  )
+        if len( keys ) == 1 :
+            #print( "len(keys) : 1")
+            return self._dict[ keys[ 0 ]]
+        elif self._dict.has_key( keys[ 0 ]) :
+                nested = self._dict[ keys[ 0 ]]
+        else:
+            raise ValueError( "nested key :'%s' does not exist in keys %s of %s" % ( keys[ 0 ], keys, self._dict ))
+        
+        if isinstance( nested, dict ) :
+            nested = NestedDict( nested )
+            return nested.get_value( keys[ 1 ] )
+        else:
+            return nested
+    
+    def has_key( self, k ):
+        
+        keys = k.split( ".", 1 )
+        nested = None
+        if len( keys ) == 1 :
+            return self._dict.has_key( keys[ 0 ] )
+        elif self._dict.has_key( keys[ 0 ]) :
+                nested = self._dict[ keys[ 0 ]]
+        else:
+            raise ValueError( "nested key :'%s' does not exist" % keys[ 0 ])
+        
+        if isinstance( nested, dict ) :
+            nested = NestedDict( nested )
+            return nested.has_key( keys[ 1 ] )
+        else:
+            return True
+                      
+        
+    def set_value( self, k, v ):
+        
+        keys = k.split( ".", 1 )
+        nested = None
+        if len( keys ) == 1 :
+            self._dict[ keys[ 0 ]] = v
+            return self
+        elif self._dict.has_key( keys[ 0 ]) :
+            nested = self._dict[ keys[ 0 ]]
+        else:
+            self._dict[ keys[ 0 ]] = {}
+            nested = self._dict[ keys[ 0 ]]
+        
+        if isinstance( nested, dict ) :
+            nested = NestedDict( nested )
+            nested.set_value( keys[ 1 ], v )
+            return self
