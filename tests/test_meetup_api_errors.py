94a9d19a2f7d35d1f12bcc500eae32cf8a8a0ea3
Joe Drumgoole <joe.drumgoole@mongodb.com>
2017-06-11 22:53:19 +0100

    Additional tests

--- tests/test_meetup_api_errors.py
+++ tests/test_meetup_api_errors.py
@@ -5,7 +5,6 @@ Created on 8 Jun 2017
 '''
 import unittest
 
-from mugalyser.meetup_api import MeetupRequest
 from mugalyser.apikey import get_meetup_key
 import requests
 
@@ -15,34 +14,40 @@ class Test(unittest.TestCase):
     def test_api_fail(self):
         params={}
         params[ "key" ] = get_meetup_key()
-        params[ "group_urlname" ] = "MongoDBGlasgow"
+        #params[ "group_urlname" ] = "MongoDBGlasgow"
+        params[ "group_urlname" ] = "London-MongoDB-User-Group"
         params[ "page"]    = 10
- 
+        count = 0
+        print( "requests.get(%s, %s)" % ( "https://api.meetup.com/2/members", params )
         r = requests.get( "https://api.meetup.com/2/members", params )
+        count = count + 1 
+        print( "requests count : %i" % count )
         print( r.url )
-        headers = r.headers
+        _ = r.headers
         body = r.json()
         
         print( "results: %i" % len( body[ "results"]))
         for i in body[ "results"]:
-            print( i )
+            #print( i )
+            pass
     
-        print( body[ "meta"])
-        count = 0
+        #print( body[ "meta"])
+        
         while ( body[ 'meta' ][ "next" ] != ""  ) :
 
-            #print( "makeRequest (old): %i" % count )
+            print( "requests.get(%s)" % body['meta'][ 'next' ] )
             r = requests.get( body['meta'][ 'next' ] )
+            count = count + 1 
+            print( "requests count : %i" % count )
             print( "next: %s" % r.url )
-            print( "body: %s" % r.text )
+            print( "body: '%s'" % r.text )
 
             body = r.json()
             print( "results: %i" % len( body ))
-            count = count + 1
-
         
             for i in body[ "results"]:
-                print( i )
+                #print( i )
+                pass
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.test_api_fail']
