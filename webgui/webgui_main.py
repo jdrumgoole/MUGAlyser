e48e1d7605a022e2a56269f08755581a0ae0360b
Ari Bender-Long <abenderlong@gmail.com>
2017-07-26 16:26:24 +0100

    Working on collection graph

--- webgui/webgui_main.py
+++ webgui/webgui_main.py
@@ -264,7 +264,7 @@ def graph():
                                             "group.name" : 1,
                                             "group.members" : 1}).sort([("group.members", -1)]).limit(int(amt))
         output = [{'Name' : d["group"]["name"], 'Count': d["group"]["members"]} for d in groupCurs]
-    else if country in ['EU', 'US', 'ALL']:
+    elif country in ['EU', 'US', 'ALL']:
         an = MUG_Analytics(mdb)
         groupList = an.get_group_names(country)
         pipeline = [
