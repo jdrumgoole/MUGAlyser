df3bfdb0c2ebdb235b39b4e1f95e16dbafd8eea0
Ari Bender-Long <abenderlong@gmail.com>
2017-07-28 09:10:54 +0100

    Added new graph

--- webgui/webgui_main.py
+++ webgui/webgui_main.py
@@ -335,10 +335,10 @@ def graph_batch():
         return redirect(url_for('show_login'))
 
     pipeline = [
-        {"$group": {"_id": "$batchID", "total_members": {"$sum": "$group.member_count"}, 'timestamp': {'$first' : '$timestamp'} }}
+        {"$group": {"_id": "$batchID", "total_members": {"$add": ["$group.member_count", "$group.members"]}, 'timestamp': {'$first' : '$timestamp'} }}
     ]
 
-    groupCurs = proGrpCollection.aggregate(pipeline)
+    groupCurs = groupCollection.aggregate(pipeline)
     output = [{'Batch' : d['_id'], 'Count': d['total_members'], 'Time': d['timestamp']} for d in groupCurs]
 
     return render_template("graphbatch.html", members = output)
