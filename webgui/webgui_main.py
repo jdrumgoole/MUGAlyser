80b693a315fa18fea67246d2d7e1626510df3482
Ari Bender-Long <abenderlong@gmail.com>
2017-07-28 14:54:19 +0100

    Modified group page

--- webgui/webgui_main.py
+++ webgui/webgui_main.py
@@ -129,7 +129,7 @@ def groups():
     
     output = []
     for d in curGroups:
-        output.append( [d["group"]["name"], "{:,}".format(d["group"]["member_count"]), round(d["group"]["average_age"], 2)])
+        output.append( [d["group"]["name"], "{:,}".format(d["group"]["member_count"]), round(d["group"]["average_age"], 2), d["group"]["last_event"]])
         
     return render_template("groups.html", groups = output)
 
