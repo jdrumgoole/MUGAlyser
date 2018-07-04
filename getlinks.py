766abb1d04adbd55f54232da72cf277656b2457b
Monica Neivandt <monicaneivandt@Monicas-MacBook-Pro.local>
2018-07-04 14:13:22 +0100

    Write results to two files, one for exact matches on list of keywords, one for all results

--- getlinks.py
+++ getlinks.py
@@ -3,30 +3,30 @@ import argparse
 import requests
 
 parser = argparse.ArgumentParser()
-parser.add_argument("searchfor")
+parser.add_argument("searchfor", nargs="*", help="list of search strings, the first to be used in meetup.com, the rest to further filter results")
 args = parser.parse_args()
 
-searchfor = args.searchfor.replace(" ","+")
-
-r = requests.get("https://www.meetup.com/find/?allMeetups=false&keywords="+searchfor+ "&radius=Infinity&userFreeform=Dublin%2C+Ireland&mcId=z1017818&sort=recommended&eventFilter=mysugg")
+r = requests.get("https://www.meetup.com/topics/"+args.searchfor[0]+"/all/")
 
 links = re.findall('"((http|ftp)s?://.*?)"', r.text)
 links_list = [i[0] for i in links]
 
 invalid = ("find", "None", "api", "url", "about", "pro", "jobs", "apps", "meetup_api", "topics", "cities", "privacy", "terms", "cookie_policy", "facebook_account_tie", "fbconnectxd_ssl.html")
 
-f = open("urls_"+searchfor+".txt", "w")
+f_exact = open("urls_"+args.searchfor[0]+"_exactmatch.txt", "w")
+f_all = open("urls_"+args.searchfor[0]+"_allresults.txt", "w")
 
 for url in links_list:
     if re.match(r"https://www\.meetup\.com/(.*)/", url):
         if url.count('/') == 4:
-            url = url.replace("https://www.meetup.com/", "").replace("/", "")
-            if url not in invalid:
-                f.write(url + '\n')
+            urlname = url.replace("https://www.meetup.com/", "").replace("/", "")
+            if urlname not in invalid:
+                if any(w in urlname.lower() for w in map(str.lower, args.searchfor)):
+                    f_exact.write(urlname + '\n')
+                f_all.write(urlname + '\n')
                 continue
     else:
         links_list.remove(url)
 
-print("url names stored in urls_"+searchfor+".txt")
-
-
+print("url names containing only search criteria stored in urls_"+args.searchfor[0]+"_exactmatch.txt")
+print("url names for all related results stored in urls_"+args.searchfor[0]+"_allresults.txt")
