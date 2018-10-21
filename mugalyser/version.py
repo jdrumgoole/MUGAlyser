82ffa5343c63b9ee0b5244154a7a5c1d2c962fee
jdrumgoole <joe@joedrumgoole.com>
2018-10-22 00:34:16 +0100

    Version bump

--- mugalyser/version.py
+++ mugalyser/version.py
@@ -4,9 +4,10 @@ Created on 11 Oct 2016
 @author: jdrumgoole
 '''
 
-__version__       = "1.0.7a1"
+__version__       = "1.0.8a0"
 __programName__   = "MUGAlyser"
 #
+# Schena Version 1.3 : Removed top level objects from each collection so now instead of member.name you use name.:wq
 # Schema version 1.1 : Date mapped the new Group fields so they appear as date time.
 #
-__schemaVersion__ = "1.2"
+__schemaVersion__ = "1.3""
