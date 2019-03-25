7d45fcdb02395b3af551192f2c1874d05f3f5dcb
Joe Drumgoole <joe.drumgoole@mongodb.com>
2019-03-25 13:13:24 +0000

    added some additonal calls to meetup_info_main

--- mugalyser/meetup_api.py
+++ mugalyser/meetup_api.py
@@ -13,16 +13,16 @@ from mugalyser.version import __programName__
 from mugalyser.meetup_request import MeetupRequest
 from mugalyser.reshape import Reshape_Event, Reshape_Pro_Group, Reshape_Group, Reshape_Member
 
-def makeRequestURL( *args ):
+
+def makeRequestURL(*args):
     url = "https://api.meetup.com"
-    
+
     for i in args:
         url = url + "/" + i
-        
+
     return url
 
 
-        
 class MeetupAPI(object):
     '''
     classdocs
@@ -32,135 +32,136 @@ class MeetupAPI(object):
         '''
         Constructor
         '''
-        
-        self._logger = logging.getLogger( __programName__)
+
+        self._logger = logging.getLogger(__programName__)
         self._api = "https://api.meetup.com/"
         self._params = {}
-        self._params[ "key" ] = apikey
-        self._params[ "page" ] = page
+        self._params["key"] = apikey
+        self._params["page"] = page
         self._reshape = reshape
         self._requester = MeetupRequest()
-    
-            
-    def get_group(self, url_name ):
 
-        ( url, _, group ) = self._requester.simple_request( self._api + url_name, params = self._params )
-        
+    def get_group(self, url_name):
+
+        (url, _, group) = self._requester.simple_request(self._api + url_name, params=self._params)
+
         if self._reshape:
-            return ( url, Reshape_Group( group ).reshape())
+            return (url, Reshape_Group(group).reshape())
         else:
-            return ( url, group)
+            return (url, group)
 
     def get_groups_by_url(self, urls):
         for i in urls:
-            yield self.get_group( i )
-            
+            yield self.get_group(i)
 
-        
-    def get_all_attendees(self, groups=None ):
+    def get_all_attendees(self, groups=None):
         groupsIterator = None
-        if groups :
+        if groups:
             groupsIterator = groups
         else:
             groupsIterator = self.get_groups()
-            
-        for group in groupsIterator :
-            return self.get_attendees(group )
-        
+
+        for group in groupsIterator:
+            return self.get_attendees(group)
+
     def get_attendees(self, url_name):
-        
-        for url, event in self.get_past_events( url_name ):
-            #pprint.pprint(event)
-            #print(event["id"])
-            for _, attendee in self.get_event_attendees(event["id"], url_name ):
-                yield (url, {"attendee" :attendee, "event":event})
-    
-            
-    def get_event_attendees(self, eventID, url_name ):
-        
-        #https://api.meetup.com/DublinMUG/events/62760772/attendance?&sign=true&photo-host=public&page=20
-        reqURL = makeRequestURL( url_name, "events", str( eventID ), "attendance")
-        return self._requester.paged_request( reqURL, self._params )
-    
-    def get_upcoming_events(self, url_name ):
-        
-        params = deepcopy( self._params )
-        params[ "status" ] = "upcoming"
-        params[ "group_urlname"] = url_name
-        
-        return self._requester.paged_request( self._api + "2/events", params )
-    
-    def get_past_events(self, url_name ) :
-        
-        params = deepcopy( self._params )
-        
-        params[ "status" ]       = "past"
-        params[ "group_urlname"] = url_name
-        
+
+        for url, event in self.get_past_events(url_name):
+            # pprint.pprint(event)
+            # print(event["id"])
+            for _, attendee in self.get_event_attendees(event["id"], url_name):
+                yield (url, {"attendee": attendee, "event": event})
+
+    def get_event_attendees(self, eventID, url_name):
+
+        # https://api.meetup.com/DublinMUG/events/62760772/attendance?&sign=true&photo-host=public&page=20
+        reqURL = makeRequestURL(url_name, "events", str(eventID), "attendance")
+        return self._requester.paged_request(reqURL, self._params)
+
+    def get_upcoming_events(self, url_name):
+
+        params = deepcopy(self._params)
+        params["status"] = "upcoming"
+        params["group_urlname"] = url_name
+
+        return self._requester.paged_request(self._api + "2/events", params)
+
+    def get_past_events(self, url_name):
+
+        params = deepcopy(self._params)
+
+        params["status"] = "past"
+        params["group_urlname"] = url_name
+
         if self._reshape:
-            return ( (url, Reshape_Event( i ).reshape()) for (url, i) in self._requester.paged_request( self._api + "2/events", params ))
+            return ((url, Reshape_Event(i).reshape()) for (url, i) in
+                    self._requester.paged_request(self._api + "2/events", params))
         else:
-            return self._requester.paged_request( self._api + "2/events", params )
+            return self._requester.paged_request(self._api + "2/events", params)
 
-    def get_member_by_id(self, member_id ):
+    def get_member_by_id(self, member_id):
 
-        ( url, _, body ) = self._requester.simple_request( self._api + "2/member/" + str( member_id ), params = self._params )
-    
+        (url, _, body) = self._requester.simple_request(self._api + "2/member/" + str(member_id), params=self._params)
+
         if self._reshape:
-            return ( url, Reshape_Member( body ).reshape())
+            return (url, Reshape_Member(body).reshape())
         else:
             return (url, body)
-    
-    def get_members(self , urls ):
+
+    def get_members(self, urls):
         for i in urls:
-            #print( "processing: %s" % i )
-            for (url, member) in self.__get_members( i ):
-                #print("processing: %s" % member[ "name"])
+            # print( "processing: %s" % i )
+            for (url, member) in self.__get_members(i):
+                # print("processing: %s" % member[ "name"])
                 yield (url, member)
-                
-    def __get_members(self, url_name ):
-        
-        params = deepcopy( self._params )
-        params[ "group_urlname" ] = url_name
- 
+
+    def __get_members(self, url_name):
+
+        params = deepcopy(self._params)
+        params["group_urlname"] = url_name
+
         if self._reshape:
-            return ((url, Reshape_Member( i ).reshape()) for (url, i) in self._requester.paged_request( self._api + "2/members", params ))
+            return ((url, Reshape_Member(i).reshape()) for (url, i) in
+                    self._requester.paged_request(self._api + "2/members", params))
         else:
-            return self._requester.paged_request( self._api + "2/members", params )
-    
-    def get_groups(self ):
+            return self._requester.paged_request(self._api + "2/members", params)
+
+    def get_groups(self):
         '''
         Get all groups associated with this API key.
         '''
-        self._logger.debug( "get_groups")
-        
-        if self._reshape :
-            return ((url, Reshape_Group( i ).reshape()) for (url, i) in self._requester.paged_request(self._api + "self/groups",  self._params ))
+        self._logger.debug("get_groups")
+
+        if self._reshape:
+            return ((url, Reshape_Group(i).reshape()) for (url, i) in
+                    self._requester.paged_request(self._api + "self/groups", self._params))
         else:
-            return self._requester.paged_request(self._api + "self/groups",  self._params )
-        
-    def get_pro_groups(self  ):
+            return self._requester.paged_request(self._api + "self/groups", self._params)
+
+    def get_pro_groups(self):
         '''
         Get all groups associated with this API key.
         '''
-        self._logger.debug( "get_pro_groups")
-        
+        self._logger.debug("get_pro_groups")
+
         if self._reshape:
-            return ((url, Reshape_Pro_Group( i ).reshape()) for (url, i) in self._requester.paged_request( self._api + "pro/MongoDB/groups", self._params ))
+            return ((url, Reshape_Pro_Group(i).reshape()) for (url, i) in
+                    self._requester.paged_request(self._api + "pro/MongoDB/groups", self._params))
         else:
-            return self._requester.paged_request( self._api + "pro/MongoDB/groups", self._params )
-        
-    def get_pro_group_names( self ):
-        for (url, i) in self.get_pro_groups() :
-            #print("pro name:'{}'".format(i["urlname"]))
+            return self._requester.paged_request(self._api + "pro/MongoDB/groups", self._params)
+
+    def get_pro_group_names(self):
+        for (url, i) in self.get_pro_groups():
+            # print("pro name:'{}'".format(i["urlname"]))
             yield i["urlname"]
-            
-    def get_pro_members(self ):
-        
-        self._logger.debug( "get_pro_members")
-        #print( self._params )
-        
+
+    def get_pro_members(self):
+
+        self._logger.debug("get_pro_members")
+        # print( self._params )
+
         if self._reshape:
-            return ((url, Reshape_Member( i ).reshape()) for (url, i) in self._requester.paged_request( self._api + "pro/MongoDB/members", self._params ))
+            return ((url, Reshape_Member(i).reshape()) for (url, i) in
+                    self._requester.paged_request(self._api + "pro/MongoDB/members", self._params))
         else:
-            return self._requester.paged_request( self._api + "pro/MongoDB/members", self._params )
+            return self._requester.paged_request(self._api + "pro/MongoDB/members", self._params)
--- mugalyser/meetup_info_main.py
+++ mugalyser/meetup_info_main.py
@@ -16,114 +16,141 @@ from mugalyser.meetup_api import MeetupAPI
 from traceback import print_exception
 from mugalyser.mugdata import printCursor
 from mugalyser.apikey import get_meetup_key
-    
-program_name = "meetup_info_main"
-    
 
-def main( ) :
-        
-    try :
-        parser = ArgumentParser( description = "A direct interface to the meetup API")
-        
-        parser.add_argument( "--apikey", default="", help="API Key to use for Calls")
-        parser.add_argument( "-i", "--member_id", type=int, help="Retrieve information for a specific ID")
-        parser.add_argument( "-g", "--mugs", nargs="+", help="Get Info for MUG")
-        parser.add_argument( "--members", default=False, action="store_true",  help="list all members of a list of groups")
-        parser.add_argument( "-l", "--listgroups", action="store_true", default=False, help = "List all groups")
-        parser.add_argument( "--groups", default=False, action="store_true",  help="list group data for groups")
-        parser.add_argument( "-u", "--urlnames", action="store_true", default=False, help = "List all groups by URL name")
-        parser.add_argument( "--pastevents", nargs="+", default=[], help="Get past events for MUG")
-        parser.add_argument( "--upcomingevents", nargs="+", default=[], help="Get upcoming events for MUG")
-        parser.add_argument( "--attendees", nargs="+", default=[], help="Get attendees for list of groups")
-        parser.add_argument( "--loop", type=int, default=1, help="Loop call for --loop times")
-        parser.add_argument( "--reshape", default=False, action="store_true", help="Reshape output for BSON" )
-        parser.add_argument( "--req", default=False, action="store_true", help="Report underlying request URL to meetup")
+program_name = "meetup_info_main"
+
+
+def main():
+    try:
+        parser = ArgumentParser(description="A direct interface to the meetup API")
+
+        parser.add_argument("--apikey", default="", help="API Key to use for Calls")
+        parser.add_argument("-i", "--member_id", type=int, help="Retrieve information for a specific ID")
+        parser.add_argument("-g", "--mugs", nargs="+", help="Get Info for MUG")
+        parser.add_argument("--members", default=False, action="store_true",
+                            help="list all members of a list of groups")
+        parser.add_argument("-l", "--listgroups", action="store_true", default=False, help="List all groups")
+        parser.add_argument("--groups", default=False, action="store_true", help="list group data for groups")
+        parser.add_argument("--allgroups", default=False, action="store_true", help="list group data for all groups for this API key")
+        parser.add_argument("--progroups", default=False, action="store_true", help="list group data for all pro groups for this API key")
+        parser.add_argument("-u", "--urlnames", action="store_true", default=False, help="List all groups by URL name")
+        parser.add_argument("--pastevents", nargs="+", default=[], help="Get past events for MUG")
+        parser.add_argument("--upcomingevents", nargs="+", default=[], help="Get upcoming events for MUG")
+        parser.add_argument("--attendees", nargs="+", default=[], help="Get attendees for list of groups")
+        parser.add_argument("--loop", type=int, default=1, help="Loop call for --loop times")
+        parser.add_argument("--reshape", default=False, action="store_true", help="Reshape output for BSON")
+        parser.add_argument("--req", default=False, action="store_true", help="Report underlying request URL to meetup")
         # Process arguments
         args = parser.parse_args()
-        
+
         format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-        logging.basicConfig(format=format_string, level=logging.INFO )
-        if args.apikey == "" :
-            m = MeetupAPI( apikey= get_meetup_key(), reshape=args.reshape )
+        logging.basicConfig(format=format_string, level=logging.INFO)
+        if args.apikey == "":
+            m = MeetupAPI(apikey=get_meetup_key(), reshape=args.reshape)
         else:
-            m = MeetupAPI( apikey = args.apikey, reshape=args.reshape )
-            
-        for i in range( args.loop ):
-            if args.member_id :
-                (url, member) = m.get_member_by_id( args.member_id )
+            m = MeetupAPI(apikey=args.apikey, reshape=args.reshape)
+
+        for i in range(args.loop):
+            if args.member_id:
+                (url, member) = m.get_member_by_id(args.member_id)
                 if args.req:
                     print("req: '{}'".format(url))
-                pprint.pprint( member )
-                
-            if args.groups :
+                pprint.pprint(member)
+
+            if args.allgroups:
+                member_total = 0
+                for group_count, g in enumerate(m.get_groups(), 1):
+                    #pprint.pprint(g)
+                    #print(f"{g[1]['urlname']}")
+                    group = g[1]
+                    full_group = m.get_group(group['urlname'])[1]
+                    if full_group["organizer"]["id"] == args.member_id:
+                        pprint.pprint(full_group)
+                        print(f"{full_group['urlname']}, {full_group['members']}")
+                        member_total = member_total + full_group['members']
+                print(f"{group_count} groups in total")
+                print(f"Total members: {member_total}")
+
+            if args.progroups:
+                member_total = 0
+                for group_count, g in enumerate(m.get_pro_groups(), 1):
+                    #pprint.pprint(g)
+                    #print(f"{g[1]['urlname']}")
+                    group = g[1]
+                    full_group = m.get_group(group['urlname'])[1]
+                    if full_group["organizer"]["id"] == args.member_id:
+                        pprint.pprint(full_group)
+                        print(f"{full_group['urlname']}, {full_group['members']}")
+                        member_total = member_total + full_group['members']
+                print(f"{group_count} groups in total")
+                print(f"Total members: {member_total}")
+            if args.groups:
                 for i in args.mugs:
-                    (url, mug)= m.get_group( i )
+                    (url, mug) = m.get_group(i)
                     if args.req:
                         print("req: '{}'".format(url))
-                    pprint.pprint( mug )
-                
-            if args.members :
-                print( "args.members: %s" % args.mugs )
-                #it = m.get_members( args.mugs )
-    
-                count = 0 
-                name=""
-                mid=""
-                for (url, j) in m.get_members( args.mugs ):
+                    pprint.pprint(mug)
+
+            if args.members:
+                print("args.members: %s" % args.mugs)
+                # it = m.get_members( args.mugs )
+
+                count = 0
+                name = ""
+                mid = ""
+                for (url, j) in m.get_members(args.mugs):
                     if args.req:
                         print("req: '{}'".format(url))
                     count = count + 1
 
                     if "name" in j:
-                        name = j[ "name"]
-                        mid = j[ "id" ]
+                        name = j["name"]
+                        mid = j["id"]
                     else:
-                        name = j[ "member_name"]
-                        mid = j[ "member_id"]
-                    print( u"{:30}, {:20}, {:20}, {:20}".format( name, i, j[ "country" ], mid ) )
-    
-                print( "%i total" % count )
-            
-            if args.pastevents :
-                ( url, past_events)  = m.get_past_events( args.pastevents )
+                        name = j["member_name"]
+                        mid = j["member_id"]
+                    print(u"{:30}, {:20}, {:20}, {:20}".format(name, i, j["country"], mid))
+
+                print("%i total" % count)
+
+            if args.pastevents:
+                (url, past_events) = m.get_past_events(args.pastevents)
                 if args.req:
                     print("req: '{}'".format(url))
-                printCursor( past_events )
-                
-            if args.upcomingevents :
-                (url, upcoming_events)= m.get_upcoming_events( args.upcomingevents )
+                printCursor(past_events)
+
+            if args.upcomingevents:
+                (url, upcoming_events) = m.get_upcoming_events(args.upcomingevents)
                 if args.req:
                     print("req: '{}'".format(url))
-                printCursor( upcoming_events )
-                
-            if args.attendees :
-                attendees = m.get_all_attendees( args.attendees )
-                printCursor( attendees )
-                
-            if args.listgroups :
-                printCursor( m.get_pro_groups())
-                
-            if args.urlnames :
+                printCursor(upcoming_events)
+
+            if args.attendees:
+                attendees = m.get_all_attendees(args.attendees)
+                printCursor(attendees)
+
+            if args.listgroups:
+                printCursor(m.get_pro_groups())
+
+            if args.urlnames:
                 for i in m.get_pro_group_names():
-                    print( i )
+                    print(i)
 
 
     except KeyboardInterrupt:
         print("Keyboard interrupt : Exiting...")
-        sys.exit( 2 )
+        sys.exit(2)
 
     except Exception as e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
-        print_exception( exc_type, exc_value, exc_traceback )
-        indent = len( "mug_info_main" ) * " "
+        print_exception(exc_type, exc_value, exc_traceback)
+        indent = len("mug_info_main") * " "
         sys.stderr.write("mug_info_main" + ": " + repr(e) + "\n")
         sys.stderr.write(indent + "  for help use --help\n")
         return 2
-        
+
     return 0
 
-if __name__ == '__main__':
-    sys.exit( main())
 
-        
-    
+if __name__ == '__main__':
+    sys.exit(main())
