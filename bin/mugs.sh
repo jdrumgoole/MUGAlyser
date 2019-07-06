c3bd543e6264e6e7c2c374c80455feb8bcfd2132
jdrumgoole <joe@joedrumgoole.com>
2019-07-06 20:53:53 +0100

    Removed broken groups, fixed script

--- bin/mugs.sh
+++ bin/mugs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python mugalyser_main.py --admin --collect all --urlfile ../etc/master_mug_list.txt $*
+python mugalyser_main.py --admin --collect all --organizer_id 99473492 --urlfile ../etc/master_mug_list.txt $*
